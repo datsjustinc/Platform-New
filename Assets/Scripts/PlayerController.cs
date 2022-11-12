4bec16c88d0ec2541e6a092e9c0ee1b4061effec
datsjustinc <jc9957@nyu.edu>
2022-11-12 16:48:02 -0500

    Bug Fixes
    1. Patch to spotlight shining through ground and various wall objects.
    
    2. Drone spawns correctly with player respawn without disabling animation and movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -361,7 +361,7 @@ public class PlayerController : MonoBehaviour
         if (teleport)
         {
             gameObject.transform.position = spawn.transform.position;
-            //drone.transform.position = new Vector3(spawn.transform.position.x -5, spawn.transform.position.y + 5, spawn.transform.position.z);
+            drone.transform.position = new Vector3(spawn.transform.position.x -5, spawn.transform.position.y + 5, spawn.transform.position.z);
             moveModel.hspeed = 0;
             playerRB.velocity = new Vector2(0, 0);
             teleport = false;
@@ -379,7 +379,7 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator Delay()
     {
-        yield return new WaitForSeconds(0.7f);
+        yield return new WaitForSeconds(0.3f);
         moveModel.hspeed = defaultspeed;
     }
 
